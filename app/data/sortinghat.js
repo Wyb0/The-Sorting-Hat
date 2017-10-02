99b538f918d15272fe9d5bdd144743b1d2ea4b75
Wyb0 <andrewwybo@gmail.com>
2017-10-01 21:30:34 -0400

    added additional questions and submit button to survey html, updated answer array

--- app/data/sortinghat.js
+++ app/data/sortinghat.js
@@ -2,22 +2,22 @@ var house = [
     {
     "name": "Gryffindor",
     "photo": "Gryffindor.png",
-    "scores": [1,1,1,1]
+    "scores": [1,1,1,1,1]
     },
     {
     "name": "Ravenclaw",
     "photo": "Ravenclaw.png",
-    "scores": [2,2,2,2]
+    "scores": [4,4,4,4,4]
     },
     {
     "name": "Hufflepuff",
     "photo": "Hufflepuff.png",
-    "scores": [3,3,3,3]
+    "scores": [3,3,3,3,3]
     },
     {
     "name": "Slytherin",
     "photo": "Slytherin.png",
-    "scores": [4,4,4,4]
+    "scores": [2,2,2,2,2]
         }
 ]
 
