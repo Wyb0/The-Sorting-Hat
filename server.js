7b9bc9985cfdfc977e3be40ec3c260222e9f629b
Andrew Wybo <andrewwybo@gmail.com>
2017-10-02 14:32:07 -0400

    server listen working

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var path = require("path")
 var express = require("express")
-var bodyParer =  require("body-parser")
+var bodyParser =  require("body-parser")
 
 var app = express()
-var PORT = 8000
+var PORT = process.env.PORT || 8000
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
 
+
+
+app.listen(PORT, function () {
+    console.log("Listening on Port: " + PORT)
+})
