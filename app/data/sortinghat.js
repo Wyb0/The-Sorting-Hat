129518bcd5cc86314e60021d627e731af5399856
Wyb0 <andrewwybo@gmail.com>
2017-10-01 20:46:18 -0400

    added array object for houses, moved pics to location of houses folder

---
+++ app/data/sortinghat.js
@@ -0,0 +1,25 @@
+var house = [
+    {
+    "name": "Gryffindor",
+    "photo": "Gryffindor.png",
+    "scores": [1,1,1,1]
+    },
+    {
+    "name": "Ravenclaw",
+    "photo": "Ravenclaw.png",
+    "scores": [2,2,2,2]
+    },
+    {
+    "name": "Hufflepuff",
+    "photo": "Hufflepuff.png",
+    "scores": [3,3,3,3]
+    },
+    {
+    "name": "Slytherin",
+    "photo": "Slytherin.png",
+    "scores": [4,4,4,4]
+        }
+]
+
+module.exports = house
+
